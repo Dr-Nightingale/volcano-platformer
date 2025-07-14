163f5db866901f5a2512dfe33ebaa2342a036034
Dr-Nightingale <Mollykiwi@icloud.com>
2025-07-15 10:33:03 +1200

    commit

--- Volcano platformer visualcode studio file.py
+++ Volcano platformer visualcode studio file.py
@@ -1,3 +1,15 @@
-print("this is where the code will go")
-print("if it works it works")
+#importing pygame module
+import pygame
 
+#initialising pygame module
+pygame.init()
+
+#setting game window size
+screen_width=800
+screen_height=500
+
+#setting the game window variable
+window=pygame.display.set_mode((screen_width, screen_height))
+
+#making a bool value to check if the game is running
+running=True
